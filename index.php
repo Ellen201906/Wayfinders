f101ac521e9a73947b5db211696a939878631bbf
Ellen201906 <51275255+Ellen201906@users.noreply.github.com>
2020-01-03 22:26:04 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,421 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Wayfinders Business Co-operative &#8211; Wayfinders Landing</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <!-- Stylesheets-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+    <link rel="stylesheet" type="text/css" href="css/yee.css">
+    <link rel="stylesheet" type="text/css" href="css/shun.css">
+    <link rel="stylesheet" type="text/css" href="css/deepa.css">
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- Javascript-->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    <!-- Page Header-->
+    <header>
+        <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3">
+            <h5 class="my-0 mr-md-auto font-weight-bold h1">Wayfinders</h5>
+            <nav class="my-2 my-md-0 mr-md-3">
+                <a class="p-2 text-white font-weight-bold h5" href="#services">Services</a>
+                <a class="p-2 text-white font-weight-bold h5" href="#about">About</a>
+                <a class="p-2 text-white font-weight-bold h5" href="#community">Community</a>
+                <a class="p-2 text-white font-weight-bold h5" href="#join">Join</a>
+                <a class="p-2 text-white font-weight-bold h5" href="#faq">FAQ</a>
+            </nav>
+            <a class="btn btn-outline-primary" href="#">Login</a>
+        </div>
+
+        <div class="header-separator header-separator-bottom ">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+                <path class="svg-white-bg" d="M737.9,94.7L0,0v100h1000V0L737.9,94.7z"></path>
+            </svg>
+        </div>
+    </header>
+
+    <!-- Section Categories-->
+    <section class="jumbotron text-center">
+        <div class="container">
+            <h2>
+                At a crossroads? Have an idea on a napkin?
+                Just starting up?
+            </h2>
+            <p class="lead text-muted">
+                We’ve been there. We know how challenging it can be to
+                start a business, run a business and grow your business.
+            </p>
+        </div>
+    </section>
+
+    <!-- Site Section -->
+    <div class="site-section">
+        <div class="container">
+
+            <div class="row justify-content-left mb-5">
+                <div class="col-md-5 aos-init aos-animate" data-aos="fade-up">
+                    <a name="services"><h2 class="section-heading text-white">We are here to help.</h2></a>
+                </div>
+            </div>
+
+            <div class="row here-to-help">
+                <div class="col-md-3 aos-init aos-animate" data-aos="fade-up" data-aos-delay="">
+                    <div class="feature-1 text-center">
+                        <div class="wrap-icon icon-1">
+                            <span class="icon la la-users"></span>
+                        </div>
+                        <img src="Icons/icon_guidance.png" alt="Guidance">
+                        <h3 class="mb-3">Guidance</h3>
+                    </div>
+                </div>
+
+                <div class="col-md-2 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+                    <div class="feature-1 text-center">
+                        <div class="wrap-icon icon-1">
+                            <span class="icon la la-toggle-off"></span>
+                        </div>
+                        <img src="Icons/icon_community.png" alt="Community">
+                        <h3 class="mb-3">Community</h3>
+                    </div>
+                </div>
+                <div class="col-md-2 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+                    <div class="feature-1 text-center">
+                        <div class="wrap-icon icon-1">
+                            <span class="icon la la-umbrella"></span>
+                        </div>
+                        <img src="Icons/icon_events.png" alt="Events">
+                        <h3 class="mb-3">Events</h3>
+                    </div>
+                </div>
+                <div class="col-md-2 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+                    <div class="feature-1 text-center">
+                        <div class="wrap-icon icon-1">
+                            <span class="icon la la-umbrella"></span>
+                        </div>
+                        <img src="Icons/icon_training.png" alt="Training">
+                        <h3 class="mb-3">Training</h3>
+                    </div>
+                </div>
+                <div class="col-md-3 aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+                    <div class="feature-1 text-center">
+                        <div class="wrap-icon icon-1">
+                            <span class="icon la la-umbrella"></span>
+                        </div>
+                        <img src="Icons/icon_cloud.png" alt="Cloud Tools">
+                        <h3 class="mb-3">Cloud Tools</h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- Body by Shun Li-->
+    <div style="position: relative; width: 100%; height: 100%;">
+        <img src="Photos\Edited-images\road_nest-edited.jpg" alt="" width="100%" />
+        <span style="position:absolute;top:40%;left:0%;font-size:50px;color:white;padding-left:300px;padding-right:200px">
+            We're building a new kind of business
+            community to support you wherever you are.
+        </span>
+    </div>
+    <br><br>
+    <h2 style="color:#004359;position:absolute;text-align:center; padding-left:500px;"><b>On the ground and in the cloud</b></h2>
+    <br><br><br> <br><br><br><br>
+    <div class="c">
+        <div><img src="Photos\Edited-images\builders-edited.jpg" alt="" /></div>
+        <div><img src="Photos\Edited-images\potter-edited.jpg" alt="" /></div>
+        <div><img src="Photos\Edited-images\florist-edited.jpg" alt="" /></div>
+        <div><img src="Photos\Edited-images\market_veggies-edited.jpg" alt="" /></div>
+        <div><img src="Photos\Edited-images\cowork-group-edit.jpg" alt="" /></div>
+        <div><img src="Photos\Edited-images\laptop-edited.jpg" alt="" /></div>
+    </div>
+    <div style="background-color:#004359; text-align:left;padding-left:100px;padding-right:100px;color:white">
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+         <a name="about"><h3><b>Focus on what you do best</b></h3></a><br>
+        <h3>
+            Tired of wearing many hats? Spending too much time on accounting, not enough time on your craft?
+            Connect with a trusted network of business owners and specialists helping each other navigate the 21st century co-operatively.
+        </h3>
+        <br><br>
+    </div>
+
+    <section>
+        <a name="community"><h1>Meet our Members and partners</h1></a>
+        <div class="logo-one">
+            <span><img src="Logos/P.ink-logo.png" alt="P.ink-logo" title="P.ink-logo" width="140" height="110"></span>
+            <span><img src="Logos/hanTV-2018-logo.png" alt="hanTV-2018-logo" title="hanTV-2018-logo" width="130" height="100"></span>
+            <span><img src="Logos/Spark-gallery.png" alt="Spark-gallery.png" title="Spark-gallery.png" width="110" height="110"></span>
+            <span><img src="Logos/TCNG-logo.png" alt="TCNG-logo" title="TCNG-logo" width="195" height="90"></span></br>
+        </div>
+        <div class="logo-two">
+            <span><img src="Logos/shift.jpg" alt="shift-m_logo" title="shift-m_logo" width="130" height="190"/></span>
+            <span><img src="Logos/EFCL-logo.jpg" alt="EFCL-logo" title="EFCL-logo" width="110" height="90"></span>
+            <span><img src="Logos/Caliber-insurance-logo.png" alt="Caliber-insurance-logo" title="Caliber-insurance-logo" width="180" height="70"></span>
+            <span><img src="Logos/flamingsoft-logo.jpg" alt="flamingsoft-logo" title="flamingsoft-logo" width="175" height="40"></span>
+        </div>
+    </section>
+
+    <section class="title">
+        <h2><i><b>See what they have to say...</i></b></h2>
+        <div class="grid-container">
+
+            <div class="column1">
+                <p>
+                    <i>"WayFinders is life-affirming, all-inclusive endeavor.At its core,serve communities and individuals by placing people at the centre of its technology,hile contributing to the growing tapestry of new and emerging IT business in Edmonton."</i></br>
+                    </br>
+                    </br>
+                    <b>
+                        Yuonne Chiu,Exective Director,</br>
+                        Multicultural Health Brokers Co-operative
+                    </b>
+                </p>
+            </div>
+
+            <div class="column2">
+                <p>
+                    <i>"Kindness and generosity is fundamental to the company culture of collaborative and cooperative team work, while respecting all and embracing everyone with the sense of belonging."</i></br>
+                    </br>
+                    </br>
+                    <b>
+                        Ahmad Sabetghadam,Phd.</br>
+                        University of Alberta
+                    </b>
+                </p>
+            </div>
+
+            <div class="column3">
+                <p>
+                    <i>"I believe that WayFinders Business Cooperative will provide a strong platform of Cloud tools."</i></br>
+                    </br>
+                    </br>
+                    <b>
+                        Antonio Cua,Arcitect and</br>
+                        Autodesk Certified Instructor
+                    </b>
+                </p>
+            </div>
+    </section>
+
+    <section>
+        <div class="banner"></div>
+    </section>
+
+    <!-- Body by Ye Liu-->
+    <div class="How">
+        <div class="container Howto">
+
+            <div class="row">
+                <a name="join" class="font-weight-bold col-md-7 ">How to become a Member</a>
+                <br />
+            </div>
+        </div>
+
+        <div class="container Fee">
+            <div class="row">
+                <p class="col-md-7 ">Fee structure</p>&nbsp;&nbsp;
+                <p class="col-md-3 ">Benefits</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <p class="col-md-2 font-weight-bold number">$100</p>
+                <p class="col-md-1 font-weight-bold number"><i class="fa fa-plus" aria-hidden="true"></i></p>
+                <p class="col-md-3 font-weight-bold number">$50</p>
+                <ul class="col-md-6 guidance">
+                    <li style="margin-left:150px">Guidance and Business Matchmaking</li>
+                    <li style="margin-left:150px">Training and Network Events</li>
+                    <li style="margin-left:150px">Co-op discounts on select services</li>
+                    <li style="margin-left:150px"> Access to cloud-based business tools</li>
+                    <li style="margin-left:150px">Voting share at Annual General Meeting</li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="container member">
+            <div class="row">
+                <p class="col-md-3 ">Member Share</p>
+                <p class="col-md-2 ">Annual Fee</p>
+            </div>
+        </div>
+
+        <br />
+        <div class="container">
+            <div class="row">
+                <a href="https://www.wayfindersbusinesscooperative.ca/test.commerce.v1/" class="col-lg-2 apply"><p>Apply to Join</p></a>
+
+            </div>
+        </div>
+    </div>
+    <br /> <br /> <br />
+
+    <div class="faq">
+        <br />
+        <div class="container">
+            <div class="row">
+                <a class="font-weight-bold col-md-3 " name="faq">FAQ</a>
+
+            </div>
+            <h3>(click questions to see answers)</h3>
+        </div>
+        <br />
+
+
+        <div class="container">
+
+            <div id="accordion">
+                <div class="card card1">
+                    <div class="card-header">
+                        <a class="card-link card-link1" data-toggle="collapse" href="#collapseOne">
+                            Who are we?
+                        </a>
+                    </div>
+                    <div id="collapseOne" class="collapse show" data-parent="#accordion">
+                        <div class="card-body">
+                            We’ve been there. We know how challenging it can be to
+                            start a business, run a business and grow your business.
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <div class="card card1">
+                    <div class="card-header">
+                        <a class="collapsed card-link card-link1" data-toggle="collapse" href="#collapseTwo">
+                            What can we do for you?
+                        </a>
+                    </div>
+                    <div id="collapseTwo" class="collapse" data-parent="#accordion">
+                        <div class="card-body">
+                            We’ve been there. We know how challenging it can be to
+                            start a business, run a business and grow your business.
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <div class="card card1">
+                    <div class="card-header">
+                        <a class="collapsed card-link card-link1" data-toggle="collapse" href="#collapseThree">
+                            How to join us?
+                        </a>
+                    </div>
+                    <div id="collapseThree" class="collapse" data-parent="#accordion">
+                        <div class="card-body">
+                            We’ve been there. We know how challenging it can be to
+                            start a business, run a business and grow your business.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br />
+    </div>
+
+
+    <div class="container">
+        <br />
+        <br />
+        <div class="row">
+            <p class="want font-weight-bold col-md-10">Want to support the work we're doing?</p>
+            <button class="col-md-2  btn-success donate" type="submit">Donate</button>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <br />
+    <br />
+    <div class="footer1">
+        <br />
+        <br />
+        <br />
+        <div class="container">
+            <div class="row">
+                <a href="https://www.wayfindersbusinesscooperative.ca/test.landing.v1/" class="col-md-3"><h3 style="color:white">Home</h3></a>
+                <a href="https://www.wayfindersbusinesscooperative.ca/test.landing.v1/services/" class="col-md-3"><h3 style="color:white">Services</h3></a>
+                <a class="col-md-6"><h3 style="color:white">Subscribe to our Newsletter</h3></a>
+
+            </div>
+
+        </div>
+
+
+        <div class="container">
+            <br />
+            <div class="row">
+                <a href="https://www.wayfindersbusinesscooperative.ca/test.landing.v1/about-us/" class="col-md-3"><h3 style="color:white">About Us</h3></a>
+                <a href="https://www.wayfindersbusinesscooperative.ca/test.landing.v1/community/" class="col-md-3"><h3 style="color:white">Community</h3></a>
+                <form action="./subscriber.php " method="post">
+                    <div class="input-group col-md-48 ">
+                        <input type="email" name="email" required class="form-control-lg col-lg-30" placeholder="Enter your email" style="border-radius:30px ">
+
+                        <span class="input-group-btn" style="margin-left:-60px">
+                            <button class="btnSub col-md-12 mx-3 " onclick="submitFun()" type="submit" require title="By entering your email and clicking the button, you are agreeing to receive our newsletter regularly.Easy unsubscribe links are provided in every email.">Subscribe </button>
+                          
+                        </span>
+                    </div>
+                    <script>
+                        function submitFun() {
+                            alert("Welcome to subscribe our newsletter ");
+                           
+
+                        }
+
+                    </script>
+
+                </form>
+               
+
+
+            </div>
+        </div>
+
+        <div class="container">
+            <br />
+            <div class="row">
+                <a href="https://www.wayfindersbusinesscooperative.ca/test.landing.v1/contact-us/" class="col-md-3"><h3 style="color:white">Contact us</h3></a>
+            </div>
+        </div>
+        <br />
+        <div class="container">
+            <div class="row">
+                <a href="https://www.wayfindersbusinesscooperative.ca/test.landing.v1/" class="col-md-6"><h3 style="color:white" class="font-weight-bold">Wayfinders Business Co-operative</h3></a>
+                <div class="col-md-6">
+                    <!-- Facebook -->
+                    <a href="http://www.facebook.com/wayfindersbusinessscooperative" type="button" class="btn-floating btn-fb mx-3">
+                        <i class="fa fa-facebook"> </i>
+                    </a>
+                    <!-- Twitter -->
+                    <a type="button" href="https://twitter.com/wayfinders_busi" class="btn-floating btn-tw mx-3">
+                        <i class="fa fa-twitter"> </i>
+                    </a>
+                    <!-- Linkedin -->
+                    <a type="button" href="https://www.linkedin.com/groups/13594348/" class="btn-floating btn-li mx-3">
+                        <i class="fa fa-linkedin"> </i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <a href="mailto:info@wayfinders.coop" class="col-md-3"><h5 style="color:white">info@wayfinders.coop</h5></a><br />
+            </div>
+        </div>
+
+        <br />
+        <br />
+    </div>
+
+
+</body> 
+</html>
---
+++ subscriber.php
@@ -0,0 +1,13 @@
+﻿<?php
+$data =$_POST["email"];
+
+$NewData=$data.",";
+
+$fileName = 'subscriber.txt';
+
+file_put_contents($fileName,$NewData,FILE_APPEND);
+
+?>
+<script>
+window.history.go(-1)
+</script>
