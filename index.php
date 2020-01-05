d1279aed69872621a179ca679451c36538cd93b2
Ellen201906 <51275255+Ellen201906@users.noreply.github.com>
2020-01-04 22:18:37 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 <html lang="en">
 <head>
     <title>Wayfinders Business Co-operative &#8211; Wayfinders Landing</title>
+	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta charset="utf-8">
+   
     <link rel="icon" href="images/favicon.ico" type="image/x-icon">
     <!-- Stylesheets-->
     <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
