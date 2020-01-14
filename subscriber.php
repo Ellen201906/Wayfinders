0e4529ac2a68ea0a6ac859c20715e849a30543bf
Ellen201906 <51275255+Ellen201906@users.noreply.github.com>
2020-01-13 23:29:38 -0700

    Update subscriber.php

--- subscriber.php
+++ subscriber.php
@@ -4,10 +4,19 @@ $data =$_POST["email"];
 $NewData=$data.",";
 
  include "dbcon.php";
+ $result = mysqli_query($dbh, "SELECT * FROM products");
 
 $sql="INSERT INTO products (ProdName) VALUES ('1234567889009888')";
 
-
+ $result = mysqli_query($dbh, $sql);
+	 if ($result)
+	{
+	 print("New Record was inserted");
+	}
+	else
+	{
+	 print("no success");
+	}
 
 echo $data;
 
